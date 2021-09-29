92dbf8693c288a05045d59194651a40c526f9927
timurseta <timur_seta@hotmail.com>
2021-09-29 22:01:27 +0200

    destructing assignment

---
+++ ex02/src/index.js
@@ -0,0 +1,22 @@
+const MY_CAR = {
+    wheels: 4,
+    cylinders: 4,
+    size: 2.
+};
+
+function main(car){
+    // Only change code below this line
+
+    //const cylinders = MY_CAR.cylinders;
+    //cons size = MY_CAR.size;
+    const { cylinders, size } = MY_CAR;
+    return {
+        cylinders,
+        size
+    }
+
+    // Only change code above this line
+}
+
+console.log(main(MY_CAR));
+module.exports = main;
